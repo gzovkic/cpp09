83611b81e4b3698f85d24729a3844704fb8baca4
gzovkic <g.zovkic@web.de>
2026-02-18 12:30:56 +0100

    added loop for  reading line for line ofd an file

--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -1,6 +1,30 @@
 #include "BitcoinExchange.hpp"
 
+std::string parseDate(std::string line)
+{
+    (void)line;
+    return (NULL);
+}
+
 void bitcoinExchange(std::string fileName)
 {
-    std::cout << fileName << std::endl;
+    std::ifstream inputfile(fileName);
+    if(!inputfile.is_open())
+    {
+        std::cerr << RED << "Error: could not open the file!" << RESET << std::endl;
+        return ;
+    }
+
+    std::string line;
+
+    std::getline(inputfile, line);
+    while(std::getline(inputfile, line))
+    {
+        std::string date;
+        // std::string value;
+
+        std::cout << line << std::endl;
+        date = parseDate(line);
+        // value = parseValue(line);
+    }
 }
