52195eed610c55bbaa042e97fd7d944c1ef72d62
gzovkic <g.zovkic@web.de>
2026-02-19 17:21:22 +0100

    created folder structure

---
+++ ex01/inc/RPN.hpp
@@ -0,0 +1,43 @@
+#pragma once 
+
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define RESET   "\033[37m"
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cmath>
+#include <iterator>
+#include <exception>
+#include <vector>
+#include <map>
+#include <tuple>
+#include <ctime>
+#include <regex>
+
+
+class btc
+{
+    public:
+        btc();
+        ~btc();
+        btc(const btc &other);
+        btc& operator=(const btc &other);
+
+        void addData( std::tuple<int, int, int> surdate, float value);
+        void printAllData();
+        float getbtcprice(std::tuple<int, int, int> date);
+        
+        std::map<std::tuple<int, int, int>, float> data;
+
+};
+
+void bitcoinExchange(std::string fileName);
---
+++ ex01/src/main.cpp
@@ -0,0 +1,9 @@
+#include "RPN.hpp"
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+	return (0);
+}
+
