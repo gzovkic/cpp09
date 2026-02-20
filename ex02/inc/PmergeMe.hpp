4b15c4453fdfa8608cc080de03ffc8919a3c877e
gzovkic <g.zovkic@web.de>
2026-02-20 10:46:24 +0100

    added basic folder structure

---
+++ ex02/inc/PmergeMe.hpp
@@ -0,0 +1,26 @@
+#pragma once 
+
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define RESET   "\033[37m"
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cmath>
+#include <iterator>
+#include <exception>
+#include <vector>
+#include <map>
+#include <tuple>
+#include <ctime>
+#include <regex>
+#include <stack>
+
